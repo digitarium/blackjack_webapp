34e9bf6ec05ef8d1657b00a0de0a3f63a7c9f52f
Darturium <darturium@gmail.com>
2014-03-24 00:01:36 -0400

    saving code changes

--- main.rb
+++ main.rb
@@ -4,6 +4,10 @@ require 'pry'
 
 set :sessions, true
 
+helpers do
+  def calculate_total(cards)
+    arr = cards.map{|element| element[1]}
+
 get '/' do
   if session[:player_name]
     redirect '/game'
@@ -13,18 +17,31 @@ get '/' do
 end
 
 get '/new_player' do
-  erb :new_game
+  erb :new_player
 end
 
 post '/new_player' do
-  if params[:player_name]
-
+  if params[:player_name] != ""
     session[:player_name] = params[:player_name]
-  # progress to the game
+    redirect '/game'
+  else
+    redirect '/new_player'
+  end
 end
 
 get '/game' do
   # set up initial game values
+  suits = ['H', 'D', 'C', 'S']
+  values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  session[:deck] = suits.product(values).shuffle!
+
+  # deal cards
+  session[:dealer_cards] = []
+  session[:player_cards] = []
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
 
   erb :new_game
 end
