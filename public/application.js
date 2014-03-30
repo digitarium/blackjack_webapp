6e1706374c13f8d531456ae20be613a1530f13a2
Darturium <darturium@gmail.com>
2014-03-30 00:24:00 -0400

    Saving changes

---
+++ public/application.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('#hit input').click(function() {
+        $.ajax({
+            type: 'POST',
+            url: '/game/player/hit'
+        }).done(function(msg) {
+            $('#game').replaceWith(msg);
+        });
+      return false;
+    });
+});
